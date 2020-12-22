ffa197819b3ec586464ca729dfc5e48e252f1685
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2020-12-21 16:30:35 -0800

    bloat kill
    killed bloat hopefully I didn't break anythng, that would suck

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 #imports
 import requests
 import time, json
-from mediawiki import MediaWiki
 import sys
 from ast import literal_eval
-"""this program gets information about vehicles and dumps it into files for further use"""
-wikipedia = MediaWiki()
-
-start_time = time.time()
+"""this program gets information about vehicles and dumps it into files for further use, made by github.com/jetstream0"""
 
 base_url = "https://vpic.nhtsa.dot.gov/api/vehicles"
-
 
 def get_vtypes():
   print('getting vehicle types')
@@ -34,14 +29,14 @@ def GetModelsForMakeYear(make,vtype):
   print("getting models for vtype")
   page = requests.get(base_url+"/GetModelsForMakeYear/make/{}/vehicletype/{}?format=json".format(make,vtype))
   if str(page) != "<Response [200]>":
-    return "koala"
+    return "placeholderkoala"
   try:
    models = page.json()["Results"]
    if models == dict():
      models = None
    return models
   except:
-    return "koala"
+    return "placeholderkoala"
 
 def Type_Search(vtype,filename):
   print("starting search")
@@ -51,7 +46,7 @@ def Type_Search(vtype,filename):
      man = man.rstrip()
      print("\033[92m New man:",man,"\033[00m")
      results = GetModelsForMakeYear(man,vtype=vtype)
-     if results == "koala":
+     if results == "placeholderkoala":
        continue
      print("sorting through results")
      if results != None:
@@ -75,30 +70,14 @@ def Type_Search(vtype,filename):
          model = model.rstrip()
          name = make+" "+model
          name = name.lower()
-         pvariable = False
-         if vtype == "Passenger Car" or vtype == "Truck":
-            pvariable = True
          print(str(i),"\033[94m"+model+"\033[00m")
          i+=1
          try:
+           #check to see if vehicle is already there
            vehicle_dict[name]
          except:
            type_v=result["VehicleTypeName"]
-           try:
-             print("getting images")
-             wikipage = wikipedia.page(name)
-             print(wikipage.title)
-             image = wikipage.images[0]
-           except:
-             print("image not found")
-           if pvariable:
-             url = "https://www.caranddriver.com/{}/{}".format(str(make).replace(" ","-").lower(),str(model).replace(" ","-").lower())
-             if str(requests.get(url)) == "<Response [200]>":
-               rurl = url
-               print("got review link")
-             vehicle_dict[name] = {"type":type_v,"make":make, "model":model,"image": image, "name": name,"review_link":rurl}
-           else:
-             vehicle_dict[name] = {"type":type_v,"make":make, "model":model,"image": image, "name": name}
+           vehicle_dict[name] = {"type":type_v,"make":make, "model":model,"name": name}
            f = open(filename,"w")
            f.write(str(vehicle_dict))
            f.close()
@@ -106,83 +85,9 @@ def Type_Search(vtype,filename):
   return vehicle_dict
 
 print(vtypes := get_vtypes())
-print(vtypes)
-#to get passenger cars first
-vtypes.remove('Incomplete Vehicle')
-vehicle_dict = Type_Search("Passenger Car","db/Passenger_Car.db")
-sys.exit()
 
-"""
-n = 0
-a = 0
-for vtype in vtypes:
- needs_lower = list()
- vtype = vtype.rstrip()
- filen = str("db/"+vtype+".db").rstrip().replace(" ","_")
- f = open(filen,"r")
- vehicle_dict = literal_eval(f.read())
- f.close()
- for vehicle in vehicle_dict:
-   if vehicle.islower():
-     pass
-   else:
-     n += 1
-     needs_lower.append(vehicle)
- for vehicle in needs_lower:
-    a += 1
-    lower_vehicle = vehicle.lower()
-    vehicle_dict[lower_vehicle] = vehicle_dict[vehicle]
-    del vehicle_dict[vehicle]
-    vehicle_dict[lower_vehicle]["name"] = vehicle_dict[lower_vehicle]["name"].lower()
- f = open(filen,"w")
- f.write(str(vehicle_dict))
- f.close()
-"""
-"""
- vtype = vtype.rstrip()
- filen = str("db/"+vtype+".db").rstrip().replace(" ","_")
- f = open(filen,"r")
- vehicle_dict = literal_eval(f.read())
- f.close()
- print(len(vehicle_dict))
- a = 0
- for vehicle in vehicle_dict:
-    if vehicle.islower():
-      print(vehicle)
-      break
-    else:
-     lower_vehicle = vehicle.lower()
-     vehicle_dict[lower_vehicle] = vehicle_dict[vehicle]
-     del vehicle_dict[vehicle]
-     vehicle_dict[lower_vehicle]["name"] = vehicle_dict[lower_vehicle]["name"].lower()
-     a+=1
- print(a)
- print(len(vehicle_dict))
- f = open(filen,"w")
- f.write(str(vehicle_dict))
- f.close()
-"""
 for vtype in vtypes:
  vtype = vtype.rstrip()
  filen = str("db/"+vtype+".db").rstrip().replace(" ","_")
+ #complete dictionary of vehicles from type, do whatever you want with it, but is already safely written to file
  vehicle_dict = Type_Search(vtype,filen)
- #check if image is correct
- idict = dict()
- for v in vehicle_dict:
-   if v["image"] != "Not Found" and v["name"] not in v["image"]:
-     idict[v["name"]] = {image:v["image"],name:v["name"]}
- for i in idict:
-   while True:
-     q = input("Is",i["image"],"a photo of",i["make"],i["name"]+"? (y,n)")
-     if q.lower().rstrip() == "y":
-       break
-     elif q.lower().rstrip() == "n":
-       vehicle_dict[i["name"]]["image"] = input("Paste in link for image or write Not Found.")
-       break
-   f=open(filen,"w")
-   f.write(str(vehicle_dict))
-   f.close()
-
-
-
-print("Program took", time.time() - start_time, "to run")
